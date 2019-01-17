5a8003e897770b78daadecf6f0211c1819db89df
James Goodricke <jggoodricke@gmail.com>
2019-01-17 18:52:48 +1100

    Login Page Functionality
    added form for login and script to check if login has been
    attempted previously. If so, display an error message.

--- login.php
+++ login.php
@@ -3,5 +3,24 @@
 	<title>Client Login</title>
 </head>
 <body>
-
+	<h1>Client Login</h1>
+	<?php
+		//Start Session
+		session_start();
+	
+		//If login has been unsucessfully attempted, display error message.
+		if(isset($_SESSION["login"])) {
+			 if($_SESSION["login"] == "false") {
+				//Display an error message and unset session variable (so the message won't be displayed until another unsuccessful login is attempted).
+				echo "<p>The email address or password is incorrect. Please try again.</p>";
+				unset($_SESSION["login"]);
+			 }
+		}
+	?>
+	<form action="orders.php" method="post">
+		Username: <input type="text" id="email"><br />
+		Password: <input type="password" id="password"><br />
+		<input type="submit">
+	</form>
+	<div id="result"></div>
 </body>
