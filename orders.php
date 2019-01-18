290f63903041529d949b9d6200640e0905a1a915
James Goodricke <jggoodricke@gmail.com>
2019-01-18 23:44:52 +1100

    Added Database Connectivity
    Implemented basic database connectivity and confirmed that a connection can be establisehd.

--- orders.php
+++ orders.php
@@ -7,5 +7,51 @@
 	<title>Past Orders</title>
 </head>
 <body>
-	<h1>Past Orders</h1>
+<h1>Past Orders</h1>
+	<?php
+		//Database Credentials
+		$serverName  = "localhost";
+		$dbName = "skoop";
+		$dbUsername = "root";
+		$dbPassword = "";
+		$dbTableName = "orders";
+		
+		//This message is displayed to the user if there's an error with the database query.
+		$errorMessage = "<p>Something went wrong connecting to the server. Please try refreshing the page, or contact your system administrator.</p>";
+		
+		// Create connection
+		$conn = new mysqli($serverName, $dbUsername, $dbPassword, $dbName);
+		//If connection error, notify the user.
+		if ($conn->connect_error) {
+			echo $errorMessage;
+		}
+		//If no connection error, fetch and display data.
+		else{
+			/*
+			echo"<table>
+			<tr>
+			<th>Order Date</th>
+			<th>Number</th>
+			<th>Contact Details</th>
+			<th>Shipping Status</th>
+			<th>Order Details</th>
+			</tr>";
+			//Query to be sent to Server
+			$statement = "SELECT id, number,  FROM ".$dbTableName." WHERE email = ?";
+			
+			`id`, `order_id`, `product_code`, `name`, `email`, `mobile`, `address_line_one`, `address_line_two`, `suburb`, `state`, `postcode`, `country`, `status`;
+				
+			//Prepare Query
+			if($query = $conn->prepare($statement)){
+				$query->bind_param('s', $email);
+				
+				//Execute Query
+				$query->execute();
+			}*/
+		}
+		
+
+		
+	?>
+	</table>
 </body>
