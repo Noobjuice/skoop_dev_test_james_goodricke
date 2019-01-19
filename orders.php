650db7474f664a524e22b65202bb00aac4a5b146
James Goodricke <jggoodricke@gmail.com>
2019-01-19 18:06:10 +1100

    Added Order Details Link
    Rows in the orders table should now link to corresponding order details page.
    Order details page functionality not yet implemented.

--- orders.php
+++ orders.php
@@ -5,6 +5,7 @@
 ?>
 <head>
 	<title>Past Orders</title>
+	<link rel="stylesheet" href="styles/styles.css">
 </head>
 <body>
 <h1>Past Orders</h1>
@@ -29,30 +30,114 @@
 		else{
 			//Query to be sent to Server
 			/*`id`, `order_id`, `product_code`, `name`, `email`, `mobile`, `address_line_one`, `address_line_two`, `suburb`, `state`, `postcode`, `country`, `status`*/
-			$statement = "SELECT id, name, email, mobile, address_line_one, address_line_two, suburb, state, postcode, country, status  FROM ".$dbTableName;
+			$statement = "SELECT id, order_id, name, email, mobile, address_line_one, address_line_two, suburb, state, postcode, country, status  FROM ".$dbTableName;
 				
 			//Prepare and execute Query
 			if($query = $conn->prepare($statement)){
 				//TODO: Delete This
 				//$query->bind_param('issssssssss');
 				$query->execute();
+				
+				//If Errors exist, report to user
+				if($query->errno != 0) {
+					echo $errorMessage;
+				}
+				//If no errors, put data in table
+				else {
+					$query->store_result();
+					$query->bind_result($id, $order_id, $name, $email, $mobile, $address_line_one, $address_line_two, $suburb, $state, $postcode, $country, $status);
+					
+					
+								
+					
+					echo"<table>
+							<tr>
+								<th>Order Number</th>
+								<th>Order Date</th>
+								<th>Name</th>
+								<th>Phone Number</th>
+								<th>Email</th>
+								<th>Address</th>
+								<th>Shipping Status</th>
+								<th>Order Details</th>
+							</tr>";
+					
+					while($query->fetch()){
+						
+						//Concatonate Address
+						$fullAddress = "";
+						//Address line 1
+						if($address_line_one != ""){
+							$fullAddress .= $address_line_one.", ";
+							
+							//Add comma if there is still more address to come
+							if($address_line_two != "" || $suburb != "" || $state != "" || $postcode != "" || $country != "") {
+								$fullAddress .= ", ";
+							}
+						}	
+						//Address line 2					
+						if($address_line_two != ""){
+							$fullAddress .= $address_line_two.", ";
+							
+							//Add comma if there is still more address to come
+							if($suburb != "" || $state != "" || $postcode != "" || $country != "") {
+								$fullAddress .= ", ";
+							}
+						}
+						//Suburb						
+						if($suburb != ""){
+							$fullAddress .= $suburb.", ";
+							
+							//Add comma if there is still more address to come
+							if($state != "" || $postcode != "" || $country != "") {
+								$fullAddress .= ", ";
+							}
+						}						
+						//State	
+						if($state != ""){
+							$fullAddress .= $state.", ";
+							
+							//Add comma if there is still more address to come
+							if($postcode != "" || $country != "") {
+								$fullAddress .= ", ";
+							}
+						}						
+						//Postcode	
+						if($postcode != ""){
+							$fullAddress .= $postcode.", ";
+							
+							//Add comma if there is still more address to come
+							if($country != "") {
+								$fullAddress .= ", ";
+							}
+						}						
+						//Country
+						if($country != ""){
+							$fullAddress .= $country;
+						}
+						
+						/*Populate row of table*/
+						echo "<tr>
+								<th>".$order_id."</th>
+								<th></th>
+								<th>".$name."</th>
+								<th>".$mobile."</th>
+								<th>".$email."</th>
+								<th>".$fullAddress."</th>
+								<th>".$status."</th>
+								<th><a href='order-details.php?id=".$id."' class='button'>&rarr;</a></th>
+							</tr>";
+							/*TODO: Delete This
+							<button onClick='parent.location=\"order-details.php?id=".$id."\"'>&rarr;</button>*/
+							
+					
+
+					}
+					//Close Table
+					echo"</table>";
+				}
 			}
-			
-			/*
-			echo"<table>
-			<tr>
-			<th>Order Date</th>
-			<th>Number</th>
-			<th>Contact Details</th>
-			<th>Shipping Status</th>
-			<th>Order Details</th>
-			</tr>";
-			*/
-
 		}
-		
-
-		
 	?>
 	</table>
 </body>
