53330c87c3d03bd268a887aa724997fac8f3d5e7
James Goodricke <jggoodricke@gmail.com>
2019-01-20 00:10:33 +1100

    Created Upload Page
    No functionality implemented yet.

---
+++ upload.php
@@ -0,0 +1,13 @@
+<HTML XMLns="http://www.w3.org/1999/xHTML">
+<?php
+	//Check user is logged in
+	include "login-check.php";
+?>
+<head>
+	<title>Upload</title>
+</head>
+<body>
+<h1>Upload Orders</h1>
+<p>DESCRIPTION HERE</p>
+
+</body>
